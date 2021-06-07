351cbd647fb3b0b5d1f29d3d9413f7dfa19766a3
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-06-07 19:46:59 +0200

    Update main_local.js

--- js/main_local.js
+++ js/main_local.js
@@ -98,11 +98,11 @@ function checkControlQuestion(){
 			}
 		}
 		if (goodAnswer){
-			document.querySelector('input[name="Options"]:checked').checked = false;
-			
 			answers.id.push(document.getElementById('postid').value);
 			answers.type.push(document.getElementById('type').value);
-			answers.rate.push(document.querySelector('input[name="Options"]:checked').value)
+			answers.rate.push(document.querySelector('input[name="Options"]:checked').value);
+			
+			document.querySelector('input[name="Options"]:checked').checked = false;
 
 			document.getElementById('control').classList.remove('button');
 			document.getElementById('control').hidden = true;
