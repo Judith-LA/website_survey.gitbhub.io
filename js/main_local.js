a3d7530d4db7d56a39b88cae481b7a8b07b31081
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-06-07 16:35:36 +0200

    Update main_local.js

--- js/main_local.js
+++ js/main_local.js
@@ -42,7 +42,7 @@ function add_controlQuestion(){
 	var comment_title = document.getElementById("title");
 	var comment_text = document.getElementById("text");
 
-	comment_id.value = controlQuest.id;
+	comment_id.value = controlQuest["id"];
 	comment_type.value = controlQuest.type;
 
 	var title = controlQuest.title;
