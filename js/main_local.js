401dd45747383fd5657913e76e988f3d90ba168c
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-08-09 10:24:06 +0200

    Update main_local.js

--- js/main_local.js
+++ js/main_local.js
@@ -15,7 +15,7 @@ var answers = {id: [],
 var startDate;
 var endDate;
 var prod;
-const uuidv4 = require("uuid/v4");
+//const uuidv4 = require("uuid/v4");
 
 function toSurvey() {
  	location.replace("survey.html");
@@ -144,7 +144,7 @@ function checkControlQuestion(){
 			displayInfo();
 		} else {
 			location.href = "fail_survey.html";
-			document.getElementById('uuidv4').textContent = uuidv4();
+			document.getElementById('uuidv4').textContent = Date.now() + Math.random();
 		}
 	}
 	
