f440ca2213b128d6a46a75f3fd560c255d739c57
Judith-LA <60671234+Judith-LA@users.noreply.github.com>
2021-10-29 14:20:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ function displayControlQuestion(){
 		var data_file = "survey_test_control_question.json";
 	}*/
 	
-	var data_file = "surveys/survey_2.json"
+	var data_file = "surveys/survey_3.json"
 
 	$.getJSON(data_file).done(function(data) {  
 		file = data;
